cddca3d9ef4bfb68eed69f8980df31a14182cf27
ddomke-class <damondomke@gmail.com>
2024-04-08 15:49:23 -0600

    guru model finished

--- sportsShare/models.py
+++ sportsShare/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Guru(models.Model):
@@ -8,7 +9,19 @@ class Guru(models.Model):
         ('Bar Owner', 'Matchup Stats'),
         ('Analyst', 'Team Comparison'),
         ('Gambler', 'Single Player Stats'),
+        ('Newbie', 'Exploring a New World'),
     )
     name = models.CharField(max_length=200)
     email = models.CharField("Email", max_length=200)
-    major = models.CharField(max_length=200, choices=TYPEGURU, blank = True)
+    experience = models.PositiveSmallIntegerField(default=0)
+    Type = models.CharField(max_length=200, choices=TYPEGURU, default='Newbie')
+
+    #Define default String to return the name for representing the Model object."
+    def __str__(self):
+        return self.name
+
+    #Returns the URL to access a particular instance of MyModelName.
+    #if you define this method then Django will automatically
+    # add a "View on Site" button to the model's record editing screens in the Admin site
+    def get_absolute_url(self):
+        return reverse('guru-detail', args=[str(self.id)])
