9110924dc352dc90ee2f162bf16e4ab76603db32
ddomke-class <damondomke@gmail.com>
2024-04-08 15:32:53 -0600

    guru model and superuser created

--- sportsShare/models.py
+++ sportsShare/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Guru(models.Model):
+
+#List of choices for major value in database, human readable name
+    TYPEGURU = (
+        ('Bar Owner', 'Matchup Stats'),
+        ('Analyst', 'Team Comparison'),
+        ('Gambler', 'Single Player Stats'),
+    )
+    name = models.CharField(max_length=200)
+    email = models.CharField("Email", max_length=200)
+    major = models.CharField(max_length=200, choices=TYPEGURU, blank = True)
