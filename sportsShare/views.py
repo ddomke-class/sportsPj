b6b77a44f7f57f1bba79e3776938fbbef3e4bfab
ddomke-class <damondomke@gmail.com>
2024-04-08 19:38:00 -0600

    navigate page up with buttons. Error with displaying statsheets

--- sportsShare/views.py
+++ sportsShare/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from .models  import *
 # Create your views here.
 def index(request):
 
@@ -7,6 +9,8 @@ def index(request):
    return render( request, 'sportsShare/index.html')
 
 def myStatsheets(request):
-
-# Render the HTML template index.html with the data in the context variable.
+   listSheets = Statsheet.objects.all()
+   print("active Stats. query set", listSheets)
    return render( request, 'sportsShare/myStatsheets.html')
+
+
